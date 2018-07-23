c2e73d98b694567f6648522de8941ee90f307b5e
Anuj Sharma <anujshama4u@gmail.com>
2018-07-23 12:31:44 +0530

    Added vue chartkick library

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
    {{title}}
+   <geo-chart :data="[['United States', 44], ['Germany', 23], ['Brazil', 22]]"></geo-chart>
+
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
+import VueChartkick from "vue-chartkick";
 import router from "./router";
 import App from "./App";
 
 Vue.use(router);
-
+Vue.use(VueChartkick);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
